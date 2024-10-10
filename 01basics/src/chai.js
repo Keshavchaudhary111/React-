a513b0a74789652feccd8a1f5717f6c89f332911
keshav chaudhary <keshav75652@gmail.com>
2024-10-10 15:49:50 +0530

    first commit

---
+++ 01basics/src/chai.js
@@ -0,0 +1,6 @@
+function Chai() {
+  return(
+    <h1>chai and react with me</h1>
+  )
+}
+export default Chai
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+
+function App() {
+  return (
+    <>
+   <h1>learning react with vite</h1>
+   <p>keshav</p>
+  <Chai/>
+  </>
+  )
+}
+
+export default App
