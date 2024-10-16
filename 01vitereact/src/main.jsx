66ade2adf622d8ef255509b3626be16ebe901c01
keshav chaudhary <keshav75652@gmail.com>
2024-10-16 08:54:02 +0530

    useState

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// const reactElement = {
+//   type : 'a',
+//    props: {
+//         href: 'http://google.com',
+//         target: '_blank',
+//      },
+//   children: 'click me to visit google'
+
+// }
+// it will not exexcute bcoz react excepts us to give a element but we provide it element
+
+// for object
+// const reactElement = React.createElement(
+//    'a',
+//    {href: 'http://www.google.com',target: '_blank'},
+//        'click me to visit google'
+// )
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {/* reactElement */}
   </StrictMode>,
 )
